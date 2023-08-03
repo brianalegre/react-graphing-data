9e17c4a4b847cad1ea9bc003148596ef7d30dd3f
Brian Alegre <brialegre@yahoo.com>
2023-08-02 22:02:22 -0700

    testing mapping out data from api in chart component

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,70 @@
-import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+// Import
+import React, { useState, useEffect } from 'react';
+import ChartComponent from './components/ChartComponent';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const App = () => {
-  const data = {
-    labels: ["Item 1", "Item 2"],
-    datasets: [
-      {
-        label: "Item 1",
-        data: [82, 100],
-        borderColor: "black",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "Item 2",
-        data: [40, 80],
-        borderColor: "black",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
+// Main
+function App() {
+
+  // useState to store data from API
+  const [cdmData, setCdmData] = useState([]);
+
+  // fetch data
+  useEffect(() => {
+    fetch('http://localhost:3001/api/cdmapi/1')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('cdmapi', data)
+        setCdmData(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, []);
+
   return (
-    <Bar
-      data={data}
-      options={{
-        responsive: true,
-      }}
-    />
-  );
-};
+    <div>
+      <ChartComponent cdmData={cdmData} />
+    </div>
+  )
+}
 
-// Export 
+// Export
 export default App;
+
+
+// import { Bar } from "react-chartjs-2";
+// import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+
+// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+// const App = () => {
+//   const data = {
+//     labels: ["Item 1", "Item 2"],
+//     datasets: [
+//       {
+//         label: "Item 1",
+//         data: [82, 100],
+//         borderColor: "black",
+//         backgroundColor: "rgba(255, 99, 132, 0.5)",
+//       },
+//       {
+//         label: "Item 2",
+//         data: [40, 80],
+//         borderColor: "black",
+//         backgroundColor: "rgba(53, 162, 235, 0.5)",
+//       },
+//     ],
+//   };
+//   return (
+//     <Bar
+//       data={data}
+//       options={{
+//         responsive: true,
+//       }}
+//     />
+//   );
+// };
+
+// // Export 
+// export default App;
--- client/src/components/ChartComponent.js
+++ client/src/components/ChartComponent.js
@@ -1,136 +1,32 @@
-import React from 'react';
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
-const ChartComponent = () => {
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 
-    const options = {
-        colors: ["#1A56DB", "#FDBA8C"],
-        series: [
-            {
-                name: "Organic",
-                color: "#1A56DB",
-                data: [
-                    { x: "Mon", y: 231 },
-                    { x: "Tue", y: 122 },
-                    { x: "Wed", y: 63 },
-                    { x: "Thu", y: 421 },
-                    { x: "Fri", y: 122 },
-                    { x: "Sat", y: 323 },
-                    { x: "Sun", y: 111 },
-                ],
-            },
-            {
-                name: "Social media",
-                color: "#FDBA8C",
-                data: [
-                    { x: "Mon", y: 232 },
-                    { x: "Tue", y: 113 },
-                    { x: "Wed", y: 341 },
-                    { x: "Thu", y: 224 },
-                    { x: "Fri", y: 522 },
-                    { x: "Sat", y: 411 },
-                    { x: "Sun", y: 243 },
-                ],
-            },
-        ],
-        chart: {
-            type: "bar",
-            height: "320px",
-            fontFamily: "Inter, sans-serif",
-            toolbar: {
-                show: false,
-            },
-        },
-        plotOptions: {
-            bar: {
-                horizontal: false,
-                columnWidth: "70%",
-                borderRadiusApplication: "end",
-                borderRadius: 8,
-            },
-        },
-        tooltip: {
-            shared: true,
-            intersect: false,
-            style: {
-                fontFamily: "Inter, sans-serif",
-            },
-        },
-        states: {
-            hover: {
-                filter: {
-                    type: "darken",
-                    value: 1,
-                },
-            },
-        },
-        stroke: {
-            show: true,
-            width: 0,
-            colors: ["transparent"],
-        },
-        grid: {
-            show: false,
-            strokeDashArray: 4,
-            padding: {
-                left: 2,
-                right: 2,
-                top: -14
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        legend: {
-            show: false,
-        },
-        xaxis: {
-            floating: false,
-            labels: {
-                show: true,
-                style: {
-                    fontFamily: "Inter, sans-serif",
-                    cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
-                }
-            },
-            axisBorder: {
-                show: false,
-            },
-            axisTicks: {
-                show: false,
-            },
-        },
-        yaxis: {
-            show: false,
-        },
-        fill: {
-            opacity: 1,
-        },
-    }
+export default function ChartComponent({ cdmData }) {
 
+    // const data = cdmData.map((item) => (
+    //     <div>
+    //         <p>{item}</p>
+    //     </div>
+    // ))
+
+    const inventoryData = cdmData.map((item) => (
+        <tbody key={item.system_id} className="bg-white divide-y divide-gray-200 text-center">
+            <tr>
+                <td className="px-6 py-4 whitespace-nowrap">
+                    {item}
+                </td>
+            </tr>
+        </tbody >
+    ))
 
     return (
         <div>
-            <div class="mx-auto w-3/5 overflow-hidden">
-                <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-                    <div class="flex justify-between pb-4 mb-4 border-b border-gray-200 dark:border-gray-700">
-                        <div class="flex items-center">
-                            <div>
-                                <h5 class="leading-none text-2xl font-bold text-gray-900 dark:text-white pb-1">Crystal Disk Mark 8</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="column-chart"></div>
-                    <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-                        <div class="flex justify-between items-center pt-5">
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+            {/* {data} */}
+            {inventoryData}
         </div>
-    );
-};
+    )
 
-export default ChartComponent;
+}

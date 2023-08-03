4587db1ffd09cf6fff8cdcbb8f1eca84c4fba869
Brian Alegre <brialegre@yahoo.com>
2023-08-02 21:40:20 -0700

    Testing react-chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
-function App() {
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const App = () => {
+  const data = {
+    labels: ["Item 1", "Item 2"],
+    datasets: [
+      {
+        label: "Item 1",
+        data: [82, 100],
+        borderColor: "black",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Item 2",
+        data: [40, 80],
+        borderColor: "black",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <h1 className='bg-red-500'> Hello</h1>
-
-    </div>
+    <Bar
+      data={data}
+      options={{
+        responsive: true,
+      }}
+    />
   );
-}
+};
 
+// Export 
 export default App;
